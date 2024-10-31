aeb22ca96325a9f4500c01367a9bf1630daec6be
shantanu-shanto <pbsilent0177@gmail.com>
2024-10-31 17:45:56 +0600

    site

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// Initialize AOS (Animate On Scroll)
+AOS.init({
+    duration: 800, // Duration of animations in milliseconds
+    once: false, // Animation should happen every time you scroll down
+});
